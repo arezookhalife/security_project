e67bd166856aa31d8b7554ac0204899a51d05bcd
arezoo <arezoo.kh1988@gmail.com>
2025-09-23 00:03:59 +0330

    DAY 4: implement custom login, signup and 2FA OTP with pyotp

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import login_view, home_view, logout_view, profile_api
+from .views import login_view, home_view, logout_view, profile_api, otp_verify, register
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('home/', home_view, name='home'),
     path('logout/', logout_view, name='logout'),
     path('api/profile/', profile_api, name='profile_api'),
+    path('register/', register, name='register'),
+    path('otp-verify/', otp_verify, name='otp_verify'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,35 @@
-from django.contrib.auth import login, logout
+import pyotp
+from django.contrib.auth import login, logout, get_user_model
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib import messages
+from .models import UserOTP
 
+User = get_user_model()
 
 def login_view(request):
     if request.method == "POST":
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
-            return redirect("home")
+            request.session["pre_otp_user_id"] = user.id
+            otp_obj, created = UserOTP.objects.get_or_create(user=user)
+            if created:
+                otp_obj.secret = pyotp.random_base32()
+                otp_obj.save()
+            totp = pyotp.TOTP(otp_obj.secret, interval=60)
+            otp_code = totp.now()
+            print(f"🔑 OTP برای {user.username}: {otp_code}")
+            return redirect('otp_verify')
         else:
             messages.error(request, "نام کاربری یا رمز عبور اشتباه است.")
     else:
         form = LoginForm()
-    return render(request, "accounts/login.html", {"form": form})
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 @login_required
@@ -36,3 +46,49 @@ def logout_view(request):
 @permission_classes([IsAuthenticated])
 def profile_api(request):
     return Response({'username': request.user.username, 'email': request.user.email})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def generate_otp(user):
+    otp_obj, created = UserOTP.objects.get_or_create(user=user)
+    if not otp_obj.secret:
+        otp_obj.secret = pyotp.random_base32()
+        otp_obj.save()
+    totp = pyotp.TOTP(otp_obj.secret)
+    print("کد OTP فعلی:", totp.now())
+    return totp.now()
+
+
+def otp_verify(request):
+    user_id = request.session.get("pre_otp_user_id")
+    if not user_id:
+        return redirect("login")
+
+    user = User.objects.get(id=user_id)
+    otp_obj = UserOTP.objects.get(user=user)
+    totp = pyotp.TOTP(otp_obj.secret, interval=60)
+
+    if request.method == 'POST':
+        code = request.POST['otp']
+
+        if totp.verify(code):
+            login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+            del request.session["pre_otp_user_id"]
+            return redirect('home')
+        else:
+            return render(request, 'accounts/otp_verify.html', {'error': 'کد اشتباه است'})
+
+    otp_code = totp.now()
+    print(f"🔄 کد جدید OTP برای {user.username}: {otp_code}")
+
+    return render(request, "accounts/otp_verify.html")
